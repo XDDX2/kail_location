fbc487bc897810874edc67e8ec3e067aa7a3b1d3
noellegazelle6 <noellegazelle6@gmail.com>
2026-09-13 18:24:43 -0700

    r8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,7 +36,16 @@ android {
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+        debug {
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
